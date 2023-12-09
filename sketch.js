460bccf755a7919b24ee801bb9a1884cb6642f24
MeriemLafsahi670 <mlafsahi63@gmal.com>
2023-12-09 09:56:07 +0100

    commit

--- sketch.js
+++ sketch.js
@@ -1,107 +1,115 @@
+//sketch
 let pursuer1, pursuer2;
-let target;
-let obstacles = [];
-let vehicules = [];
-let vehicle;
-let cibles = [];
-let demo = "snake";
+  let target;
+  let obstacles = [];
+  let vehicules = [];
+  let vehicle;
+  let cibles = [];
+  let demo = "snake";
+  
+  
+  let imgVaisseau;
+  
+  function preload() {
+    console.log("preload")
+    imgVaisseau = loadImage('assets/images/vaisseau.png');
+  }
+  
+  function setup() {
+    console.log("setup")
+    createCanvas(windowWidth, windowHeight);
+    pursuer1 = new Vehicle(100, 100, imgVaisseau);
+    pursuer2 = new Vehicle(random(width), random(height), imgVaisseau);
+  
+  
+    vehicules.push(pursuer1);
+    vehicules.push(pursuer2);
+   
+    // On cree un obstace au milieu de l'écran
+    // un cercle de rayon 100px
+    // TODO
+    obstacles.push(new Obstacle(width / 2, height / 2, 100));
+  }
+  
+  function draw() {
+    // changer le dernier param (< 100) pour effets de trainée
+    background(0, 0, 0, 100);
+  
+    target = createVector(mouseX, mouseY);
+  
+    // Dessin de la cible qui suit la souris
+    // Dessine un cercle de rayon 32px à la position de la souris
+    fill(255, 0, 0);
+    noStroke();
+    circle(target.x, target.y, 32);
+   
+  
+    // dessin des obstacles
+    // TODO
+    obstacles.forEach(o => {
+      o.show();
+    })
 
-let imgVaisseau;
 
-function preload() {
-  console.log("preload")
-  imgVaisseau = loadImage('assets/images/vaisseau.png');
-}
-
-function setup() {
-  console.log("setup")
-  createCanvas(windowWidth, windowHeight);
-  pursuer1 = new Vehicle(100, 100, imgVaisseau);
-  pursuer2 = new Vehicle(random(width), random(height), imgVaisseau);
-
-  vehicules.push(pursuer1);
-  vehicules.push(pursuer2);
-
-  // On cree un obstace au milieu de l'écran
-  // un cercle de rayon 100px
-  // TODO
-  obstacles.push(new Obstacle(width / 2, height / 2, 100));
-}
-
-function draw() {
-  // changer le dernier param (< 100) pour effets de trainée
-  background(0, 0, 0, 100);
-
-  target = createVector(mouseX, mouseY);
-
-  // Dessin de la cible qui suit la souris
-  // Dessine un cercle de rayon 32px à la position de la souris
-  fill(255, 0, 0);
-  noStroke();
-  circle(target.x, target.y, 32);
-
-  // dessin des obstacles
-  // TODO
-  obstacles.forEach(o => {
-    o.show();
-  })
-  switch (demo) {
-    
-  case "snake":
-    vehicules.forEach((vehicle, index) => {
-      let forceArrive;
-      vehicle.poidsSeparation = 0;
-
-      if (index == 0) {
-        // C'est le 1er véhicule, il suit la cible/souris
-        forceArrive = vehicle.applyBehaviors(target, obstacles, vehicules, 0);
-      } else{
-        // les véhicules suivants suivent le véhicule précédent
-        let vehiculePrecedent = vehicules[index - 1];
-
-        forceArrive = vehicle.applyBehaviors(vehiculePrecedent.pos, obstacles, vehicules, 40);
-      }
+    switch (demo) {
       
-
-      vehicle.update();
-      vehicle.show();
-    });
-    break;
-
-  }
-
- 
-}
-function mousePressed() {
-  // TODO : ajouter un obstacle de taille aléatoire à la position de la souris
-  obstacles.push(new Obstacle(mouseX, mouseY, random(30, 100)));
-
-}
-
-
-
-
-function keyPressed() {
-  // quand on clique sur la lettre v il nous ajoute une seule vehicule.
-  if (key == "v") {
-    vehicules.push(new Vehicle(random(width), random(height), imgVaisseau));
-  }
-  if (key == "d") {
-    Vehicle.debug = !Vehicle.debug;
-  }
-  if(key == "l"){
-     demo = "snake";
-  }
-
-  //quand on clique sur la lettre f il nous ajoute plusieur vehicules.
-  if (key == "f") {
-    // Je cree 100 vehicules qui partent du bord gauche de l'écran
-    // et qui vont vers la cible/souris
-    for (let i = 0; i < 10; i++) {
-      let v = new Vehicle(random(10, 20), random(height/2 -10,height/2 +10 ), imgVaisseau)
-      v.maxSpeed = 10;
-      v.color = "purple";
-      vehicules.push(v);
+    case "snake":
+      vehicules.forEach((vehicle, index) => {
+        let forceArrive;
+        vehicle.poidsSeparation = 0;
+  
+        if (index == 0) {
+          // C'est le 1er véhicule, il suit la cible/souris
+          forceArrive = vehicle.applyBehaviors(target, obstacles, vehicules, 0);
+        } else{
+          // les véhicules suivants suivent le véhicule précédent
+          let vehiculePrecedent = vehicules[index - 1];
+  
+          forceArrive = vehicle.applyBehaviors(vehiculePrecedent.pos, obstacles, vehicules, 40);
+        }
+        
+  
+        vehicle.update();
+        vehicle.show();
+      });
+      break;
+  
     }
+  
+   
   }
-}
+  function mousePressed() {
+    // TODO : ajouter un obstacle de taille aléatoire à la position de la souris
+    obstacles.push(new Obstacle(mouseX, mouseY, random(30, 100)));
+  
+  }
+  
+  
+  
+  
+  function keyPressed() {
+    // quand on clique sur la lettre v il nous ajoute une seule vehicule.
+    if (key == "v") {
+      vehicules.push(new Vehicle(random(width), random(height), imgVaisseau));
+    }
+    if (key == "d") {
+      Vehicle.debug = !Vehicle.debug;
+    }
+    if(key == "l"){
+       demo = "snake";
+    }
+  
+    //quand on clique sur la lettre f il nous ajoute plusieur vehicules.
+    if (key == "f") {
+      // Je cree 100 vehicules qui partent du bord gauche de l'écran
+      // et qui vont vers la cible/souris
+      for (let i = 0; i < 10; i++) {
+        let v = new Vehicle(random(10, 20), random(height/2 -10,height/2 +10 ), imgVaisseau)
+        v.maxSpeed = 10;
+        v.color = "purple";
+        vehicules.push(v);
+      }
+    }
+  
+  
+  }
